f8f8dd414f19ecb5dabcf0128e2dff8d0c589f27
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-02-07 17:33:30 +0000

    Actualización de archivos en funcionamiento

--- scripts.js
+++ scripts.js
@@ -1,34 +1,91 @@
 let imagenes = [];
 let selecciones = [];
+let scoreTime = 0;
+let timerWorking = true;
 
-generarTablero();
+document.querySelector("#start-btn").onclick = generarTablero;
+
 
 function cargarImagenes() {
     imagenes = [
-        '<img class="front-face" src="/img/perrito.png" alt="perrito">',
-        '<img class="front-face" src="/img/conejo.png" alt="conejo">',
-        '<img class="front-face" src="/img/patito.png" alt="patito">',
-        '<img class="front-face" src="/img/mono.png" alt="mono">',
-        '<img class="front-face" src="/img/tortuga.png" alt="tortuga">',
-        '<img class="front-face" src="/img/castor.png" alt="castor">',
+        '<img src="/img/ardilla.png" width="80%">',
+        '<img src="/img/castor.png" width="80%">',
+        '<img src="/img/conejo.png" width="80%">',
+        '<img src="/img/gatito.png" width="80%">',
+        '<img src="/img/mono.png" width="80%">',
+        '<img src="/img/patito.png" width="80%">',
+        '<img src="/img/perrito.png" width="80%">',
+        '<img src="/img/tortuga.png" width="80%">',
     ]
 }
 
 function generarTablero() {
     cargarImagenes();
+    let len = imagenes.length;
+    selecciones = [];
     let tablero = document.getElementById("tablero");
     let tarjetas = [];
-    for (let i = 0; i < 12; i++) {
-        tarjetas.push(`<div class="memory-card" id="tarjeta${i}">
-                                ${imagenes[0]}
-                            
-                                <img class="back-face" src="/img/gatito.png" alt="Memory Card">
-                            
-                        </div>`);
+
+    for (let i = 0; i < len * 2; i++) {
+        tarjetas.push(`
+                <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
+                    <div class="tarjeta" id="tarjeta${i}">
+                        <div class="cara trasera" id="trasera${i}">
+                            ${imagenes[0]}
+                        </div>
+                        <div class="cara superior">
+                        <img src="/img/interrogacion.png" width="80%">
+                        </div>
+                    </div>
+                </div>        
+                `)
         if (i % 2 == 1) {
-            imagenes.splice(0, 1)
+            imagenes.splice(0, 1);
         }
     }
-    tarjetas.sort(() => Math.random() - 1);
+    tarjetas.sort(() => Math.random() - 0.5);
     tablero.innerHTML = tarjetas.join(" ");
+    timerReset();
+}
+
+function seleccionarTarjeta(i) {
+    let tarjeta = document.getElementById("tarjeta" + i);
+    if (tarjeta.style.transform != "rotateY(180deg)") {
+        tarjeta.style.transform = "rotateY(180deg)";
+        selecciones.push(i);
+    }
+    if (selecciones.length == 2) {
+        deseleccionar(selecciones);
+        selecciones = [];
+    }
+}
+
+function deseleccionar(selecciones) {
+    setTimeout(() => {
+        let trasera1 = document.getElementById("trasera" + selecciones[0]);
+        let trasera2 = document.getElementById("trasera" + selecciones[1]);
+        if (trasera1.innerHTML != trasera2.innerHTML) {
+            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0]);
+            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1]);
+            tarjeta1.style.transform = "rotateY(0deg)";
+            tarjeta2.style.transform = "rotateY(0deg)";
+        } else {
+            trasera1.style.background = "black";
+            trasera2.style.background = "black";
+        }
+    }, 1000);
+}
+
+// TIMER
+function timer() {
+    document.getElementById("scoreTime").innerHTML = scoreTime;
+    scoreTime++;
+}
+
+setInterval(() => {
+    if (timerWorking) { timer() }
+}, 1000)
+
+function timerReset() {
+    scoreTime = 0;
 }
