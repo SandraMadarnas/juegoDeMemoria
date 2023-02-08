0e3eb44c66f9873c763e0144ced564cb9bf1a9f6
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-02-08 23:58:09 +0000

    Actualizando scripts del juego

--- scripts.js
+++ scripts.js
@@ -1,24 +1,35 @@
+// const timer = document.querySelector('#timer');
+// const cards = document.querySelectorAll('.card');
+
+const score = document.querySelector('#score');
+let time = 0;
+let timerId;
+
 let imagenes = [];
 let selecciones = [];
 let scoreTime = 0;
 let timerWorking = true;
 
+// const startBtn = document.querySelector('#startBtn');
+// startBtn.addEventListener('click', generarTablero);
 document.querySelector("#start-btn").onclick = generarTablero;
 
 
+// CARGAR IMÁGENES
 function cargarImagenes() {
     imagenes = [
-        '<img src="/img/ardilla.png" width="80%">',
+        '<img src="/img/ardilla.png" width="100%">',
         '<img src="/img/castor.png" width="80%">',
-        '<img src="/img/conejo.png" width="80%">',
-        '<img src="/img/gatito.png" width="80%">',
-        '<img src="/img/mono.png" width="80%">',
+        '<img src="/img/conejo.png" width="100%">',
+        '<img src="/img/gatito.png" width="100%">',
+        '<img src="/img/mono.png" width="100%">',
         '<img src="/img/patito.png" width="80%">',
-        '<img src="/img/perrito.png" width="80%">',
+        '<img src="/img/perrito.png" width="100%">',
         '<img src="/img/tortuga.png" width="80%">',
     ]
 }
 
+// GENERAR TABLERO
 function generarTablero() {
     cargarImagenes();
     let len = imagenes.length;
@@ -48,6 +59,7 @@ function generarTablero() {
     timerReset();
 }
 
+// DAR VUELTA A LAS TARJETAS, SI HACEN MACTH SE QUEDAN DE CARA, SI NO SE DAN LA VUELTA
 function seleccionarTarjeta(i) {
     let tarjeta = document.getElementById("tarjeta" + i);
     if (tarjeta.style.transform != "rotateY(180deg)") {
@@ -76,7 +88,8 @@ function deseleccionar(selecciones) {
     }, 1000);
 }
 
-// TIMER
+
+// TIEMPO
 function timer() {
     document.getElementById("scoreTime").innerHTML = scoreTime;
     scoreTime++;
@@ -89,3 +102,35 @@ setInterval(() => {
 function timerReset() {
     scoreTime = 0;
 }
+
+
+// PUNTOS
+function updateScore(match) {
+    // Estoy llamando a una variable que esta dentro de una funcion
+    if (trasera1.innerHTML != trasera2.innerHTML) {
+        score++;
+    } else {
+        score--;
+    }
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+// Example usage:
+// updateScore(true);  // increases the score by 1
+
+
+let cardsLeft = 8;
+
+function updateScore(match) {
+    if (match) {
+        cardsLeft--;
+        if (cardsLeft === 0) {
+            alert("¡Felicidades! Has completado el juego de memoria.");
+        }
+    }
+}
+
+
+
+// MENSAJE DE JUEGO TERMINADO
+// let juegoFinalizado = window.confirm('🎉 ¡Has terminado el juego!');
